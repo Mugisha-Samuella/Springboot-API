94ca77ec4d76fbf48091e6526ad4deac535fa1c3
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-09-10 14:09:31 +0200

    Employee Repository & ResourceNotFoundException

--- src/main/java/com/restapi/spring_crud/api/model/User.java
+++ src/main/java/com/restapi/spring_crud/api/model/User.java
@@ -24,4 +24,6 @@ public class User {
     @Column (name = "email")
     private String email;
 
+
+
 }
---
+++ src/main/java/com/restapi/spring_crud/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.restapi.spring_crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository {
+    //All crud operations
+}

8af04c9d2fe6c2e801f84b58378e131254177d73
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-09-25 20:59:29 +0200

    Employee Service

--- src/main/java/com/restapi/spring_crud/api/controller/EmployeeController.java
+++ src/main/java/com/restapi/spring_crud/api/controller/EmployeeController.java
@@ -1,111 +1,55 @@
 package com.restapi.spring_crud.api.controller;
 
 import com.restapi.spring_crud.api.model.Employee;
-import com.restapi.spring_crud.exception.ResourceNotFoundException;
-import com.restapi.spring_crud.repository.EmployeeRepository;
+import com.restapi.spring_crud.service.EmployeeService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.BadRequestException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
 
-    private final EmployeeRepository employeeRepository;
+    public final EmployeeService employeeService;
+
 
     @GetMapping
     public List<Employee> getAllEmployees(){
-        return employeeRepository.findAll();
+        return employeeService.getAllEmployees();
     }
 
     @GetMapping("/test")
     public String test(){
         System.out.println("Hello from frontend.");
-        return "Samuellla";
+        return "Samuella";
     }
 
     @PostMapping
-    public ResponseEntity<String> createEmployee(@RequestBody Employee employee)throws Exception{
-
-
-        ResponseEntity<String> validationResponse = validateEmployee(employee);
-        if(validationResponse != null){
-                  return validationResponse;
-        }
-
-        Optional<Employee> employeeOptional = employeeRepository.findByEmail(employee.getEmail());
-        if(employeeOptional.isPresent()){
-            throw new BadRequestException("Employee already exists.");
-        }
-        employeeRepository.save(employee);
-
-        return ResponseEntity.ok("Employee created successfully!");
-    }
-
-    private ResponseEntity<String> validateEmployee(@RequestBody Employee employee){
-        List<String> validationMessages = new ArrayList<>();
-
-        if(isNullOrEmpty(employee.getFirstName())){
-            validationMessages.add("First name is required! \n");
-        }
-        if(isNullOrEmpty(employee.getLastName())){
-            validationMessages.add("Last name is required! \n");
-        }
-        if(isNullOrEmpty(employee.getEmployeeTitle())){
-            validationMessages.add("Employee Title is required! \n");
-        }
-        if(isNullOrEmpty(employee.getEmail())){
-            validationMessages.add("Email is required! \n");
-        }
-
-        if(!validationMessages.isEmpty()){
-            return ResponseEntity.ok(String.join(" ", validationMessages));
-        }
-
-        return null;
-    }
-
-    private boolean isNullOrEmpty(String value){
-        return value == null || value.isEmpty() || value.equals("");
+    public ResponseEntity<String> createEmployee(@RequestBody Employee employee) throws Exception {
+        return employeeService.createEmployee(employee);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
-        Employee employee =employeeRepository.findById(id)
-                .orElseThrow(()-> new ResourceNotFoundException("Employee with id " + id + " not found"));
+        Employee employee = employeeService.getEmployeeById(id);
         return ResponseEntity.ok(employee);
     }
 
     @Transactional
     @PutMapping("{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employeeDetails){
-        Employee updateEmployee = employeeRepository.findById(id)
-                .orElseThrow(() -> new  ResourceNotFoundException("Employee with id " + id + " not found"));
-
-        updateEmployee.setFirstName(employeeDetails.getFirstName());
-        updateEmployee.setLastName(employeeDetails.getLastName());
-        updateEmployee.setEmail(employeeDetails.getEmail());
-        updateEmployee.setEmployeeTitle(employeeDetails.getEmployeeTitle());
-
-        employeeRepository.save(updateEmployee);
+        Employee updateEmployee = employeeService.updateEmployee(id, employeeDetails).getBody();
         return ResponseEntity.ok(updateEmployee);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id){
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow(()-> new ResourceNotFoundException("Employee with id " + id + " not found"));
-
-        employeeRepository.delete(employee);
+        employeeService.deleteEmployeeByID(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
--- src/main/java/com/restapi/spring_crud/service/EmployeeService.java
+++ src/main/java/com/restapi/spring_crud/service/EmployeeService.java
@@ -1,22 +1,99 @@
 package com.restapi.spring_crud.service;
 
 import com.restapi.spring_crud.api.model.Employee;
+import com.restapi.spring_crud.exception.ResourceNotFoundException;
 import com.restapi.spring_crud.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
-    @Autowired
-    public EmployeeRepository employeeRepository;
+
+    private final EmployeeRepository employeeRepository;
 
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(long id){
+        return employeeRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee with id " + id + " not found!"));
+    }
+
+    public ResponseEntity<String> createEmployee(@RequestBody Employee employee)throws Exception{
+
+        ResponseEntity<String> validationResponse = validateEmployee(employee);
+        if(validationResponse != null){
+            return validationResponse;
+        }
+
+        Optional<Employee> employeeOptional = employeeRepository.findByEmail(employee.getEmail());
+        if(employeeOptional.isPresent()){
+            throw new BadRequestException("Employee already exists.");
+        }
+        employeeRepository.save(employee);
+
+        return ResponseEntity.ok("Employee created successfully!");
+    }
+
+    private ResponseEntity<String> validateEmployee(@RequestBody Employee employee){
+        List<String> validationMessages = new ArrayList<>();
+
+        if(isNullOrEmpty(employee.getFirstName())){
+            validationMessages.add("First name is required! \n");
+        }
+        if(isNullOrEmpty(employee.getLastName())){
+            validationMessages.add("Last name is required! \n");
+        }
+        if(isNullOrEmpty(employee.getEmployeeTitle())){
+            validationMessages.add("Employee Title is required! \n");
+        }
+        if(isNullOrEmpty(employee.getEmail())){
+            validationMessages.add("Email is required! \n");
+        }
+
+        if(!validationMessages.isEmpty()){
+            return ResponseEntity.ok(String.join(" ", validationMessages));
+        }
+
+        return null;
+    }
+
+    private boolean isNullOrEmpty(String value){
+        return value == null || value.isEmpty() || value.equals("");
+    }
+
+
+
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employeeDetails){
+        Employee employeeUpdate = employeeRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee with id " + id + " not found!"));
+
+        employeeUpdate.setFirstName(employeeDetails.getFirstName());
+        employeeUpdate.setLastName(employeeDetails.getLastName());
+        employeeUpdate.setEmail(employeeDetails.getEmail());
+        employeeUpdate.setEmployeeTitle(employeeDetails.getEmployeeTitle());
+
+        employeeRepository.save(employeeUpdate);
+        return ResponseEntity.ok(employeeUpdate);
+    }
+
+    public ResponseEntity<HttpStatus> deleteEmployeeByID(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee with id " + id + " not found!"));
+
+        employeeRepository.delete(employee);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
